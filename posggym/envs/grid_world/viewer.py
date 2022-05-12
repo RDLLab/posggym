82bd622db4053b4b33d287ab5887d6dae9b7f083
Jonathon <Jonathon.schwartz12@gmail.com>
2022-05-12 07:01:40 -0400

    Implemented agent centric rendering support

--- posggym/envs/grid_world/viewer.py
+++ posggym/envs/grid_world/viewer.py
@@ -8,12 +8,13 @@ This implemention is based on the gym-minigrid library:
 - github.com/maximecb/gym-minigrid/blob/master/gym_minigrid/minigrid.py
 """
 import sys
-from typing import Tuple
+from typing import Tuple, Optional
 
 import numpy as np
 
 try:
     import matplotlib.pyplot as plt
+    import matplotlib.gridspec as gridspec
 except ImportError:
     print('To display the environment in a window, please install matplotlib.')
     sys.exit(-1)
@@ -22,20 +23,41 @@ except ImportError:
 class GWViewer:
     """Handles displaying grid world renders."""
 
-    def __init__(self, title: str, figsize: Tuple[int, int] = (9, 9)):
+    def __init__(self,
+                 title: str,
+                 figsize: Tuple[int, int] = (9, 9),
+                 num_agent_displays: Optional[int] = None):
         self._title = title
         self._figsize = figsize
+        self._num_agent_displays = num_agent_displays
 
-        self._fig, self._ax = plt.subplots(
-            1, 1, figsize=figsize, num=title
+        self._fig = plt.figure(
+            num=title, figsize=figsize, constrained_layout=True
         )
-        self._imshow_obj = None
 
-        # Turn off x/y axis numbering/ticks
-        self._ax.xaxis.set_ticks_position('none')
-        self._ax.yaxis.set_ticks_position('none')
-        self._ax.set_xticklabels([])
-        self._ax.set_yticklabels([])
+        if num_agent_displays is None:
+            spec = gridspec.GridSpec(1, 1, figure=self._fig)
+        else:
+            spec = gridspec.GridSpec(num_agent_displays, 2, figure=self._fig)
+
+        # Main world view is entire first column
+        self._main_ax = self._fig.add_subplot(spec[:, 0])
+        self._main_imshow_obj = None
+
+        self._agent_axs = []
+        self._agent_imshow_objs = []              # type: ignore
+        if num_agent_displays is not None:
+            for i in range(num_agent_displays):
+                ax = self._fig.add_subplot(spec[i, 1])
+                self._agent_axs.append(ax)
+                self._agent_imshow_objs.append(None)
+
+        for ax in [self._main_ax] + self._agent_axs:
+            # Turn off x/y axis numbering/ticks
+            ax.xaxis.set_ticks_position('none')
+            ax.yaxis.set_ticks_position('none')
+            ax.set_xticklabels([])
+            ax.set_yticklabels([])
 
         # Flag indicating window was closed
         self._closed = False
@@ -46,15 +68,27 @@ class GWViewer:
 
         self._fig.canvas.mpl_connect('close_event', close_handler)
 
-    def display_img(self, img: np.ndarray):
+    def display_img(self, img: np.ndarray, agent_idx: Optional[int] = None):
         """Show an image or update the image being shown."""
+        if agent_idx is None:
+            ax = self._main_ax
+            imshow_obj = self._main_imshow_obj
+        else:
+            ax = self._agent_axs[agent_idx]
+            imshow_obj = self._agent_imshow_objs[agent_idx]
+
         # Show the first image of the environment
-        if self._imshow_obj is None:
-            self._imshow_obj = self._ax.imshow(
+        if imshow_obj is None:
+            imshow_obj = ax.imshow(
                 img, interpolation='bilinear', origin='upper'
             )
 
-        self._imshow_obj.set_data(img)  # type: ignore
+            if agent_idx is None:
+                self._main_imshow_obj = imshow_obj
+            else:
+                self._agent_imshow_objs[agent_idx] = imshow_obj
+
+        imshow_obj.set_data(img)  # type: ignore
         self._fig.canvas.draw()
 
         # Let matplotlib process UI events
