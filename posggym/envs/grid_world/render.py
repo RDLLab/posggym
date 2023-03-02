a500a0b983bfd5363e83f8251b75d25fce9a2900
Jonathon <Jonathon.schwartz12@gmail.com>
2023-03-02 16:35:55 -0500

    removed random print statement

--- posggym/envs/grid_world/render.py
+++ posggym/envs/grid_world/render.py
@@ -333,7 +333,6 @@ class GWRenderer:
             highlight_obj.render(self.window_surface)
 
         if self.render_mode == "human":
-            print("update")
             pygame.event.pump()
             pygame.display.update()
             self.clock.tick(self.render_fps)
