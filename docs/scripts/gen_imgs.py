903d797d1b456c9703621563cb50774ec7cfde31
Jonathon <Jonathon.schwartz12@gmail.com>
2023-05-15 18:27:41 -0400

    Added env images and script for generating images

---
+++ docs/scripts/gen_imgs.py
@@ -0,0 +1,127 @@
+"""Generates gifs of environments.
+
+Copied and adapted from Gymnasium:
+https://github.com/Farama-Foundation/Gymnasium/blob/v0.27.0/docs/scripts/gen_gifs.py
+
+"""
+import os
+import os.path as osp
+import re
+import argparse
+from typing import List
+
+from PIL import Image
+from tqdm import tqdm
+
+import posggym
+from utils import kill_strs
+
+DOCS_DIR = osp.abspath(osp.join(osp.dirname(osp.abspath(__file__)), os.pardir))
+
+# snake to camel case:
+# https://stackoverflow.com/questions/1175208/elegant-python-function-to-convert-camelcase-to-snake-case
+pattern = re.compile(r"(?<!^)(?=[A-Z])")
+# how many steps to perform in env before saving image
+STEPS = 50
+# height of PNG in pixels, width will be scaled to ensure correct aspec ratio
+HEIGHT = 1024
+
+
+def gen_img(
+    env_id: str,
+    ignore_existing: bool = False,
+    custom_env: bool = False,
+    resize: bool = False,
+):
+    """Gen image for env."""
+    print(env_id)
+    env = posggym.make(env_id, disable_env_checker=True, render_mode="rgb_array")
+
+    # extract env name/type from class path
+    split = str(type(env.unwrapped)).split(".")
+    # get the env type (e.g. Box2D)
+    env_type = "custom_env" if custom_env else split[2]
+
+    # get rid of version info
+    env_name = env_id.split("-")[0]
+    # convert NameLikeThis to name_like_this
+    env_name = pattern.sub("_", env_name).lower()
+
+    # path for saving video
+    v_dir_path = os.path.join(DOCS_DIR, "_static", "images", env_type)
+    # create dir if it doesn't exist
+    os.makedirs(v_dir_path, exist_ok=True)
+    v_file_path = os.path.join(v_dir_path, env_name + ".png")
+
+    if os.path.exists(v_file_path) and not ignore_existing:
+        # don't overwrite existing video
+        print(
+            f"PNG image already exists for {env_name} so skipping (Use "
+            "`--ignore-existing` to overwrite existing files."
+        )
+        return
+
+    # obtain and save STEPS frames worth of steps
+    frames: List[Image] = []
+    while True:
+        env.reset()
+        done = False
+        while not done and len(frames) <= STEPS:
+            rgb_frame = env.render()  # type: ignore
+            frames.append(Image.fromarray(rgb_frame))
+            action = {i: env.action_spaces[i].sample() for i in env.agents}
+            _, _, _, _, done, _ = env.step(action)
+
+        if len(frames) > STEPS:
+            break
+
+    env.close()
+
+    frame = frames[-1]
+    if resize:
+        # h / w = H / w'
+        # w' = Hw/h
+        frame = frame.resize((HEIGHT, int(HEIGHT * frame.width / frame.height)))
+
+    # https://pillow.readthedocs.io/en/stable/handbook/image-file-formats.html#png
+    frame.save(
+        os.path.join(v_file_path),
+        dpi=(120, 120),
+    )
+    print(f"Saved: {env_name} to {v_file_path}")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+    parser.add_argument(
+        "--env-id",
+        type=str,
+        default=None,
+        help="ID of environment to run, if None then runs all registered envs.",
+    )
+    parser.add_argument(
+        "--ignore-existing",
+        action="store_true",
+        help="Overwrite existing imange if it exists.",
+    )
+    args = parser.parse_args()
+
+    if args.env_id is None:
+        # iterate through all envspecs
+        for env_spec in tqdm(posggym.envs.registry.values()):
+            if any(x in str(env_spec.id) for x in kill_strs):
+                continue
+            # try catch in case missing some installs
+            try:
+                env = posggym.make(env_spec.id, disable_env_checker=True)
+                # the gymnasium needs to be rgb renderable
+                if "rgb_array" not in env.metadata["render_modes"]:
+                    continue
+                gen_img(env_spec.id, args.ignore_existing)
+            except BaseException as e:
+                print(f"{env_spec.id} ERROR", e)
+                continue
+    else:
+        gen_img(args.env_id, args.ignore_existing)
