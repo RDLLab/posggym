890d76ee8f6408a00326679e315ec39414b01ed7
rhys newbury <newbury777@gmail.com>
2023-05-25 14:29:44 +1000

    Updated with more driving seeds

--- posggym/agents/continuous/driving/__init__.py
+++ posggym/agents/continuous/driving/__init__.py
@@ -21,13 +21,46 @@ for policy_file_name in [
     spec = PPOPolicy.get_spec_from_path(
         env_id=ENV_ID,
         env_args={
-            "grid": "14x14RoundAbout",
+            "world": "14x14RoundAbout",
             "num_agents": 2,
             "obs_dist": 5.0,
             "n_sensors": 16,
         },
         policy_file_path=osp.join(
             agent_model_dir, "driving_14x14roundabout", policy_file_name
+        ),
+        version=0,
+        valid_agent_ids=None,
+        # policy is deterministic given random seed
+        nondeterministic=False,
+        # actions sampled, rather always taking most probable action
+        deterministic=False,
+        obs_processor_cls=processors.RescaleProcessor,
+        obs_processor_config={"min_val": -1, "max_val": 1},
+        action_processor_cls=processors.RescaleProcessor,
+        action_processor_config={"min_val": -1, "max_val": 1},
+    )
+    policy_specs[spec.id] = spec
+
+
+# 7x7RoundAbout-n2-v0
+for policy_file_name in [
+    "sp_seed0.pkl",
+    "sp_seed1.pkl",
+    "sp_seed2.pkl",
+    "sp_seed3.pkl",
+    "sp_seed4.pkl",
+]:
+    spec = PPOPolicy.get_spec_from_path(
+        env_id=ENV_ID,
+        env_args={
+            "world": "7x7RoundAbout",
+            "num_agents": 2,
+            "obs_dist": 5.0,
+            "n_sensors": 16,
+        },
+        policy_file_path=osp.join(
+            agent_model_dir, "driving_7x7roundabout", policy_file_name
         ),
         version=0,
         valid_agent_ids=None,
