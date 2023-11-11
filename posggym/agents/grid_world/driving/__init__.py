2ffde58c670e636bcfeb8f58658555838c907c35
Jonathon <Jonathon.schwartz12@gmail.com>
2023-11-11 15:08:16 -0500

    updated Driving-v1 agents

--- posggym/agents/grid_world/driving/__init__.py
+++ posggym/agents/grid_world/driving/__init__.py
@@ -1,18 +1,21 @@
 """Policies for the Driving environments."""
+import os.path as osp
+
 from posggym.agents.grid_world.driving.shortest_path import DrivingShortestPathPolicy
 from posggym.agents.registration import PolicySpec
+from posggym.agents.torch_policy import PPOPolicy
+from posggym.agents.utils import processors
+from posggym.config import AGENT_MODEL_DIR
 
 
-ENV_ID = "Driving-v1"
+agent_model_dir = osp.join(AGENT_MODEL_DIR, "grid_world", "driving")
 policy_specs = {}
 
-for name, aggressiveness in [
-    ("aggressive", 1),
-    ("normal", 0.5),
-    ("conservative", 0),
-]:
+
+for aggressiveness in [0.0, 0.4, 0.6, 0.8, 1.0]:
+    name = f"A{aggressiveness*100:.0f}"
     spec = PolicySpec(
-        policy_name=f"{name}_shortestpath",
+        policy_name=f"{name}Shortestpath",
         entry_point=DrivingShortestPathPolicy,
         version=0,
         env_id="Driving-v1",
@@ -20,5 +23,38 @@ for name, aggressiveness in [
         valid_agent_ids=None,
         nondeterministic=False,
         kwargs={"aggressiveness": aggressiveness},
+    )
+    policy_specs[spec.id] = spec
+
+
+for policy_file_name in [
+    "RL0.pkl",
+    "RL1.pkl",
+    "RL2.pkl",
+    "RL3.pkl",
+    "RL4.pkl",
+]:
+    spec = PPOPolicy.get_spec_from_path(
+        env_id="Driving-v1",
+        env_args={
+            "grid": "14x14RoundAbout",
+            "num_agents": 2,
+            "obs_dim": (3, 1, 1),
+        },
+        policy_file_path=osp.join(
+            agent_model_dir,
+            "grid=14x14RoundAbout-num_agents=2-obs_dim=(3,1,1)",
+            policy_file_name,
+        ),
+        version=0,
+        valid_agent_ids=None,
+        # policy is deterministic given random seed
+        nondeterministic=False,
+        # actions sampled, rather always taking most probable action
+        deterministic=False,
+        obs_processor_cls=processors.FlattenProcessor,
+        obs_processor_config=None,
+        action_processor_cls=None,
+        action_processor_config=None,
     )
     policy_specs[spec.id] = spec
